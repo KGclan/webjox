8fb74e792d1891f79622ee8b9c22a4a5ad271668
Gorbenko_KV <gorbenko_kv@km-union.ru>
2023-03-02 19:55:34 +0300

    added css module

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,25 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import styles from "./styles.module.css";
+
 const NotFound = () => (
-  <main className="grid min-h-full place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8">
+  <main className={styles.main}>
     <div className="text-center">
-      <p className="text-base font-semibold text-indigo-600">404</p>
-      <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+      <p className={styles.error}>404</p>
+      <h1 className={styles.title}>
         Страница не найдена
       </h1>
-      <p className="mt-6 text-base leading-7 text-gray-600">
+      <p className={styles.message}>
         Извините, мы не смогли натйи данную страницу.
       </p>
-      <div className="mt-10 flex items-center justify-center gap-x-6">
+      <div className={styles.buttonsWrapper}>
         <Link
           to="/"
-          className={`
-            rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm 
-            font-semibold text-white shadow-sm hover:bg-indigo-500 
-            focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 
-            focus-visible:outline-indigo-600
-          `}>
+          className={styles.gg}
+        >
           На главную
         </Link>
       </div>
